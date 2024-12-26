161694f6c98a7cc4764bbb79b9721b2000292ea6
anarouis <anarftliyev@gmail.com>
2024-12-26 22:51:36 +0400

    Initial commit

---
+++ src/main/java/az/edu/turing/flightbookingspring/FlightBookingSpringApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flightbookingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/edu/turing/flightbookingspring/FlightBookingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flightbookingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
